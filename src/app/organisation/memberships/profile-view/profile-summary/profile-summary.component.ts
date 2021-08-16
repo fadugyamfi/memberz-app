565a812e790dc34ffbc6e07c32f1b350d9bd4dad
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2021-08-16 22:28:05 +0000

    Extracted profile summary to separate component

---
+++ src/app/organisation/memberships/profile-view/profile-summary/profile-summary.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { OrganisationMember } from '../../../../shared/model/api/organisation-member';
+
+@Component({
+  selector: 'app-profile-summary',
+  templateUrl: './profile-summary.component.html',
+  styleUrls: ['./profile-summary.component.scss']
+})
+export class ProfileSummaryComponent implements OnInit {
+
+  public _membership: OrganisationMember;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  set membership(value) {
+    this._membership = value;
+  }
+
+  get membership(): OrganisationMember {
+    return this._membership;
+  }
+
+  hasWorkInformation() {
+    return this.membership.member && this.membership.member.occupation;
+  }
+
+  hasContactInformation() {
+    return this.membership.member && (this.membership.member.email || this.membership.member.mobile_number);
+  }
+}
