c193f4458f459c1f24e419ed407bcd8a6e1a0199
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2021-09-23 20:11:03 +0000

    MEM-20 - Image upload with cropping functionality

--- src/app/organisation/memberships/profile-view/profile-view.component.ts
+++ src/app/organisation/memberships/profile-view/profile-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { OrganisationMemberService } from '../../../shared/services/api/organisation-member.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrganisationMember } from '../../../shared/model/api/organisation-member';
@@ -6,9 +6,10 @@ import { Subscription, Subject } from 'rxjs';
 import Swal from 'sweetalert2';
 import { EventsService } from '../../../shared/services/events.service';
 import { debounceTime } from 'rxjs/operators';
-import { MemberAccountService } from '../../../shared/services/api/member-account.service';
-import { MemberAccount } from '../../../shared/model/api/member-account';
-import { OrganisationService } from '../../../shared/services/api/organisation.service';
+import { MemberImageService } from '../../../shared/services/api/member-image.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MemberImage } from '../../../shared/model/api/member-image';
+import { ImageCroppedEvent, ImageCropperComponent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-profile-view',
@@ -17,11 +18,17 @@ import { OrganisationService } from '../../../shared/services/api/organisation.s
 })
 export class ProfileViewComponent implements OnInit, OnDestroy {
 
+  @ViewChild('imageCropper') imageCropper: ImageCropperComponent;
+
   private _messages = new Subject<string>();
   public alertMessage = '';
   public alertType = 'success';
   public membership: OrganisationMember;
   public active = 1;
+
+  public profileImageForm: FormGroup;
+  public profileImageUrl: any;
+  public imageUploadProgress = 0;
 
   public subscriptions: Subscription[] = [];
 
@@ -30,11 +37,13 @@ export class ProfileViewComponent implements OnInit, OnDestroy {
     public route: ActivatedRoute,
     public router: Router,
     public events: EventsService,
+    public memberImageService: MemberImageService
   ) { }
 
   ngOnInit() {
     this.setupEvents();
     this.loadProfile();
+    this.setupImageUploadEvents();
   }
 
   ngOnDestroy() {
@@ -44,12 +53,13 @@ export class ProfileViewComponent implements OnInit, OnDestroy {
   loadProfile() {
     this.membership = this.membershipService.getSelectedModel();
 
-    if ( !this.membership ) {
+    if (!this.membership) {
       const sub = this.route.params.subscribe(params => {
         const membership_id = params.id; // (+) converts string 'id' to a number
 
         const ps = this.membershipService.getProfile(membership_id).subscribe((membership: OrganisationMember) => {
           this.membership = membership;
+          this.profileImageUrl = this.membership.member.image();
           // close any open loader
           Swal.close();
         });
@@ -76,15 +86,48 @@ export class ProfileViewComponent implements OnInit, OnDestroy {
     this.events.on('OrganisationMember:updated', (profile) => {
       this.membership = profile;
 
-      if ( profile.approved && profile.active ) {
+      if (profile.approved && profile.active) {
         this.alertType = 'success';
         this._messages.next(`Registration Approved. New Membership Number: ${profile.organisation_no}`);
-      } else if ( !profile.approved && !profile.active ) {
+      } else if (!profile.approved && !profile.active) {
         this.alertType = 'danger';
         this._messages.next('Registration Rejected');
       }
     });
   }
 
+  setupImageUploadEvents() {
+    this.events.on(`MemberImage:upload:start`, () => this.imageUploadProgress = 1);
+    this.events.on(`MemberImage:upload:progress`, (value) => this.imageUploadProgress = value);
+    this.events.on('MemberImage:upload:complete', () => this.imageUploadProgress = 0);
+  }
 
+  onImageFormSubmit(e: any) {
+    e.preventDefault();
+
+    const file: File = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener('load', (ev) => {
+      this.profileImageUrl = reader.result;
+
+      const memberImage = new MemberImage({
+        member_id: this.membership.member_id,
+        image: file
+      });
+
+      this.memberImageService.createWithUpload(memberImage);
+    });
+  }
+
+  onCroppedImageSaved(image) {
+    this.profileImageUrl = image;
+
+    const memberImage = new MemberImage({
+      member_id: this.membership.member_id,
+      image_base64: image
+    });
+
+    this.memberImageService.createWithUpload(memberImage);
+  }
 }
