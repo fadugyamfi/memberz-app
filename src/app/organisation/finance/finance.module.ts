9e6c875ff03231d73a2d820631059c6fd2f1308b
Derrick Korku <derrickkorku1@gmail.com>
2021-10-15 17:31:35 +0000

    Select by year component added

--- src/app/organisation/finance/finance.module.ts
+++ src/app/organisation/finance/finance.module.ts
@@ -5,12 +5,14 @@ import { FinanceRoutingModule } from './finance-routing.module';
 import { ChartsModule } from 'ng2-charts';
 import { IncomeComponent } from './income/income.component';
 import { SelectMonthControlComponent } from 'src/app/shared/components/forms/select-month-control/select-month-control.component';
+import { SelectYearControlComponent } from 'src/app/shared/components/forms/select-year-control/select-year-control.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     IncomeComponent,
     SelectMonthControlComponent,
+    SelectYearControlComponent
   ],
   imports: [
     CommonModule,
