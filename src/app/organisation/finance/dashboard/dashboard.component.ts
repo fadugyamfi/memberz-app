78feb6df3eb3f419ec45a279bcbcb6809b97b001
Derrick Korku <derrickkorku1@gmail.com>
2021-10-26 15:27:31 +0000

    update

--- src/app/organisation/finance/dashboard/dashboard.component.ts
+++ src/app/organisation/finance/dashboard/dashboard.component.ts
@@ -1,35 +1,51 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FinanceDashboardService } from 'src/app/shared/services/api/finance-dashboard.service';
 import * as chartData from '../../../shared/data/chart/chartjs';
 import { Subscription } from 'rxjs';
-
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  public monthLabels = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", 'Oct', "Nov", "Dec"];
+  public monthLabels = chartData.monthLabels;
+  public chartColors = chartData.chartColors;
+  public barChartOptions: any = chartData.barChartOptions;
+  public barChartType = chartData.barChartType;
+
+  // lineGraph Chart
+  public lineGraphOptions = chartData.lineGraphOptions;
+  public lineGraphType = chartData.lineGraphType;
+
+
+  // Doughnut
+  public doughnutChartType = chartData.doughnutChartType;
+  public doughnutChartColors = [];
+  public doughnutChartOptions = chartData.doughnutChartOptions;
+
+
   private subscriptions: Subscription[] = [];
-  // public barChartType = 'bar';
 
   public weeklyBreakDownData = [];
   public weeklyBreakdownLabels = [];
   private weeklyBreakdownDataCurrencies = [];
+  public showWeeklybreakdownChart = false;
 
 
   public totalsByCategoryData = [];
   private totalsByCategoryDataCurrencies = [];
+  public showTotalsByCategory = false;
 
 
   public trendData = [];
   public trendCurrencies = [];
+  public showTrendChart = false;
 
   public categoryBreakdownData = [];
   public categoryBreakdownLables = [];
-  private categoryBreakdownCurrencies = [];
+  public showCategoryBreakdownChart = false;
 
   constructor(
     public reportService: FinanceDashboardService
@@ -42,7 +58,7 @@ export class DashboardComponent implements OnInit {
   }
 
   fetchWeeklyBreakdownReport() {
-    return this.reportService.getWeeklyBreakdown().subscribe((data: any[]) => {
+    const sub =  this.reportService.getWeeklyBreakdown().subscribe((data: any[]) => {
       for (let i = 0; i < data.length; i++) {
 
         /** Populate weeklybreakdown lables array */
@@ -70,15 +86,22 @@ export class DashboardComponent implements OnInit {
         }
 
         this.weeklyBreakDownData.push({
-          data: dataset, label: label
+          data: dataset, label: label,
+          backgroundColor: this.chartColors[i].bgColor,
+          borderColor: this.chartColors[i].bdColor,
+          borderwidth: this.chartColors[i].bWidth
         });
+
+        this.showWeeklybreakdownChart = true;
       }
 
     });
+
+    this.subscriptions.push(sub);
   }
 
   fetchTotalsByCategory() {
-    return this.reportService.getTotalsByCategory().subscribe((data: any[]) => {
+    const sub =  this.reportService.getTotalsByCategory().subscribe((data: any[]) => {
       for (let i = 0; i < data.length; i++) {
 
         /** Populate totalsByCategory currencies array */
@@ -91,8 +114,10 @@ export class DashboardComponent implements OnInit {
       /** Populate totalsByCategory chart data array */
       for (let i = 0; i < this.totalsByCategoryDataCurrencies.length; i++) {
         let label = this.totalsByCategoryDataCurrencies[i];
-        let trendLabel = this.trendCurrencies[i];
         let dataset = [];
+
+        /** Use totalsByCategory data for trend data */
+        let trendLabel = this.trendCurrencies[i];
         let trendDataset = [];
 
         /** Group data by {data: [...data], label: 'currency_code' } */
@@ -104,17 +129,32 @@ export class DashboardComponent implements OnInit {
         }
 
         this.totalsByCategoryData.push({
-          data: dataset, label: label
+          data: dataset, label: label,
+          backgroundColor: this.chartColors[i].bgColor,
+          borderColor: this.chartColors[i].bdColor,
+          borderwidth: this.chartColors[i].bWidth
         });
 
-        this.trendData.push({ data: trendDataset, label: trendLabel });
+        this.showTotalsByCategory = true;
+
+        /** Set chart data for trends */
+        this.trendData.push({
+          data: trendDataset, label: trendLabel,
+          backgroundColor: this.chartColors[i].bgColor,
+          borderColor: this.chartColors[i].bdColor,
+          borderwidth: this.chartColors[i].bWidth
+        });
+
+        this.showTrendChart = true;
       }
 
     });
+
+    this.subscriptions.push(sub);
   }
 
-  fetchCategoryBreakdown(){
-    return this.reportService.getCategoryBreakdown().subscribe((data: any[]) => {
+  fetchCategoryBreakdown() {
+    const sub =  this.reportService.getCategoryBreakdown().subscribe((data: any[]) => {
       for (let i = 0; i < data.length; i++) {
 
         /** Populate weeklybreakdown lables array */
@@ -124,11 +164,12 @@ export class DashboardComponent implements OnInit {
 
       }
 
+      let dataset = [];
+
       /** Populate categoryBreakdown chart data array */
       for (let i = 0; i < this.categoryBreakdownLables.length; i++) {
         let label = this.categoryBreakdownLables[i];
-        let dataset = [];
-
+      
         /** Group data by {data: [...data], label: 'contribution_type' } */
         for (let j = 0; j < data.length; j++) {
           if (data[j].contribution_type == label) {
@@ -136,45 +177,19 @@ export class DashboardComponent implements OnInit {
           }
         }
 
-        this.categoryBreakdownData.push({
-          data: dataset, label: label
-        });
       }
 
+      this.categoryBreakdownData.push(...dataset);
+      this.showCategoryBreakdownChart = true;
+
     });
+
+    this.subscriptions.push(sub);
   }
 
-  // barChart
-  public barChartOptions = chartData.barChartOptions;
-  public barChartLabels = chartData.barChartLabels;
-  public barChartType = chartData.barChartType;
-  public barChartLegend = chartData.barChartLegend;
-  public barChartData = chartData.barChartData;
-  public barChartColors = chartData.barChartColors;
-
-  // lineGraph Chart
-  public lineGraphOptions = chartData.lineGraphOptions;
-  public lineGraphLabels = chartData.lineGraphLabels;
-  public lineGraphType = chartData.lineGraphType;
-  public lineGraphLegend = chartData.lineGraphLegend;
-  public lineGraphData = chartData.lineGraphData;
-  public lineGraphColors = chartData.lineGraphColors;
-
-  // lineChart
-  public lineChartData = chartData.lineChartData;
-  public lineChartLabels = chartData.lineChartLabels;
-  public lineChartOptions = chartData.lineChartOptions;
-  public lineChartColors = chartData.lineChartColors;
-  public lineChartLegend = chartData.lineChartLegend;
-  public lineChartType = chartData.lineChartType;
-
-  // Doughnut
-  public doughnutChartLabels = chartData.doughnutChartLabels;
-  public doughnutChartData = chartData.doughnutChartData;
-  public doughnutChartType = chartData.doughnutChartType;
-  public doughnutChartColors = chartData.doughnutChartColors;
-  public doughnutChartOptions = chartData.doughnutChartOptions;
-
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 
   // events
   public chartClicked(e: any): void { }
