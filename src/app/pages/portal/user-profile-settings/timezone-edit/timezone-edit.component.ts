fe5aceb86e76d0a12804396de36c18afd552492a
Derrick Korku <derrickkorku1@gmail.com>
2021-12-30 09:30:15 +0000

    update

--- src/app/pages/portal/user-profile-settings/timezone-edit/timezone-edit.component.ts
+++ src/app/pages/portal/user-profile-settings/timezone-edit/timezone-edit.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from 'src/app/shared/services/api/auth.service';
 import { MemberAccountService } from 'src/app/shared/services/api/member-account.service';
 import { EventsService } from 'src/app/shared/services/events.service';
 import Swal from 'sweetalert2';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timezone-edit',
@@ -14,6 +15,7 @@ export class TimezoneEditComponent implements OnInit {
 
   public timeZone: any;
   private memberAccount: MemberAccount;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     public authService: AuthService,
@@ -37,7 +39,11 @@ export class TimezoneEditComponent implements OnInit {
   }
 
   setupEvents() {
-    this.events.on('MemberAccount:updated', () =>  Swal.fire('Request successful', 'Your timezone has been set successfully.', 'success'));
+    this.events.on('MemberAccount:updated', () => {
+      const sub = this.authService.me(true).subscribe();
+      this.subscriptions.push(sub);
+      Swal.fire('Request successful', 'Your timezone has been set successfully.', 'success');
+    });
   }
 
   removeEvents() {
@@ -45,7 +51,7 @@ export class TimezoneEditComponent implements OnInit {
   }
 
 
-  saveTimeZone(){
+  saveTimeZone() {
     this.memberAccount.timezone = this.timeZone.nameValue;
 
     this.memberAccountService.update(this.memberAccount);
