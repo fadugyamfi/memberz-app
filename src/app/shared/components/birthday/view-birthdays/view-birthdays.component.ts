a4f671e754d0c8d066cfdc3277841501c61d1985
Derrick Korku <derrickkorku1@gmail.com>
2022-06-15 14:20:45 +0000

    View for Find Birthdays Updated

--- src/app/shared/components/birthday/view-birthdays/view-birthdays.component.ts
+++ src/app/shared/components/birthday/view-birthdays/view-birthdays.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { Observable, Subscription, tap } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { EventsService } from '../../../../shared/services/events.service';
+import { PageEvent } from '../../../../shared/components/pagination/pagination.component';
 import { OrganisationMemberService } from 'src/app/shared/services/api/organisation-member.service';
+import { UntypedFormGroup, UntypedFormControl, Validators, UntypedFormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-view-birthdays',
@@ -9,15 +13,45 @@ import { OrganisationMemberService } from 'src/app/shared/services/api/organisat
 })
 export class ViewBirthdaysComponent implements OnInit {
 
+  public subscriptions: Subscription[] = [];
+  public selectForm: UntypedFormGroup;
+  public birthdays: any[] = [];
+
   constructor(
-    public organisationMemberService: OrganisationMemberService
+    public organisationMemberService: OrganisationMemberService,
+    public events: EventsService,
+    public translate: TranslateService
   ) { }
 
   ngOnInit(): void {
+    this.setupSelectForm();
+    this.fetchBirthdays();
   }
 
-  findBirthdays(){
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  fetchBirthdays(birthday_for = 'today', page = 1, limit = 30){
+    console.log(birthday_for)
     return [];
   }
 
+  /**
+   * Sets up the select form
+   */
+   setupSelectForm() {
+    this.selectForm = new UntypedFormGroup({
+      birthday_for: new UntypedFormControl('today')
+    });
+  }
+
+  /**
+   * Handles the pagination events
+   *
+   * @param event PageEvent
+   */
+   onPaginate(event: PageEvent) {
+    this.fetchBirthdays(this.selectForm.value, event.page, event.limit);
+  }
 }
